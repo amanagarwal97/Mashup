b699b9b502bbd352baf54eb3c213e02320e5a467
Aman Agarwal <aman.agarwal97@gmail.com>
2016-12-13 11:42:24 +0000

    Implemented database search for query

--- public/search.php
+++ public/search.php
@@ -5,7 +5,25 @@
     // numerically indexed array of places
     $places = [];
 
-    // TODO: search database for places matching $_GET["geo"], store in $places
+    // search database for places matching $_GET["geo"], store in $places
+    if(is_numeric($_GET["geo"])) {
+        $rows=CS50::query("SELECT * FROM places WHERE postal_code LIKE ?","%".$_GET["geo"]."%");
+    }
+    else if(is_string($_GET["geo"])) {
+        $rows=CS50::query("SELECT * FROM places WHERE MATCH(postal_code,place_name,admin_name1,admin_name2) AGAINST (?)",$_GET["geo"]);
+    }
+    foreach($rows as $row) {
+        $places[]=[
+            "name" => $row["place_name"],
+            "code" => $row["postal_code"],
+            "city" => $row["admin_name2"],
+            "state" => $row["admin_name1"],
+            "latitude" => $row["latitude"],
+            "longitude" => $row["longitude"]
+        ];
+    }
+    
+    
 
     // output places as JSON (pretty-printed for debugging convenience)
     header("Content-type: application/json");
